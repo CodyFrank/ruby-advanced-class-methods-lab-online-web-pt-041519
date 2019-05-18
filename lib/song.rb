2a885dfcb923476f8b19197bad2a6f320bda0067
Cody Frank <cody.frank30@gmail.com>
2019-05-18 22:48:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
   def self.new_from_filename(file_name)
     data = file_name.split(" - ")
     artist = data[0]
-    song = data[1].gsub(".mp3")
+    song = data[1].gsub(".mp3", "")
     binding.pry
   end
 end
